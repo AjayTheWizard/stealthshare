0658c05336ed9f62fdf85ea0e43b42a4c9ff1ae1
AjayTheWizard <jesualbert51@gmail.com>
2025-02-11 10:19:14 +0530

    fix: server file changes for deployment

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const { proxyRouter } = require("./server/proxyRouter");
+const { peerRouter } = require("./server/peerRouter");
+
+// const WebTorrent = (await import("webtorrent")).default;
+// const webTorrent = new WebTorrent();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/proxy", proxyRouter);
+app.use("/peer", peerRouter);
+
+app.listen(PORT, () => {
+  console.log("Server listening at http://localhost:" + PORT);
+});
